07b6273f208614a4602225a9325ebf6d686e7871
lucastraba <lucastraba@gmail.com>
2025-01-02 19:11:01 +0100

    fix: relax typescript constrains for constructible arguments

--- src/common.type.ts
+++ src/common.type.ts
@@ -1,7 +1,8 @@
 export type Nullable<T> = NonNullable<T> | null;
 export type Maybe<T> = NonNullable<T> | undefined;
 export type MaybeNullable<T> = Maybe<T> | Nullable<T>;
-export type Constructible<T = unknown> = new (...args: unknown[]) => T;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type Constructible<T = unknown> = new (...args: any[]) => T;
 export type ModuleWithDefaultExport<T = unknown> = {
   default: Constructible<T>;
 };
